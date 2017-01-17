8caad60aa99f162ffbd3b4969bc473f7a11aa2b3
FranciscoG <frankjg@gmail.com>
2017-01-17 10:49:47 -0500

    step 1, fixing lint errors, semicolons dammit

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 #!/usr/bin/env node
-"use strict"
+"use strict";
 
-const osmosis = require("osmosis")
-const chalk = require("chalk")
-const rainbow = require("chalk-rainbow")
-const twilio = require("twilio")
-const blessed = require("blessed")
-const contrib = require("blessed-contrib")
-const format = require("date-format")
-const pretty = require("pretty-ms")
-const airports = require("airports")
+const osmosis = require("osmosis");
+const chalk = require("chalk");
+const rainbow = require("chalk-rainbow");
+const twilio = require("twilio");
+const blessed = require("blessed");
+const contrib = require("blessed-contrib");
+const format = require("date-format");
+const pretty = require("pretty-ms");
+const airports = require("airports");
 
 // Time constants
-const TIME_MS = 1
-const TIME_SEC = TIME_MS * 1000
-const TIME_MIN = TIME_SEC * 60
-const TIME_HOUR = TIME_MIN * 60
+const TIME_MS = 1;
+const TIME_SEC = TIME_MS * 1000;
+const TIME_MIN = TIME_SEC * 60;
+const TIME_HOUR = TIME_MIN * 60;
 
 // Fares
-var prevLowestOutboundFare
-var prevLowestReturnFare
+var prevLowestOutboundFare;
+var prevLowestReturnFare;
 const fares = {
   outbound: [],
   return: []
-}
+};
 
 // Command line options
-var originAirport
-var destinationAirport
-var outboundDateString
-var returnDateString
-var adultPassengerCount
-var individualDealPrice
-var totalDealPrice
-var interval = 30 // In minutes
+var originAirport;
+var destinationAirport;
+var outboundDateString;
+var returnDateString;
+var adultPassengerCount;
+var individualDealPrice;
+var totalDealPrice;
+var interval = 30; // In minutes
 
 // Parse command line options (no validation, sorry!)
 process.argv.forEach((arg, i, argv) => {
   switch (arg) {
     case "--from":
-      originAirport = argv[i + 1]
-      break
+      originAirport = argv[i + 1];
+      break;
     case "--to":
-      destinationAirport = argv[i + 1]
-      break
+      destinationAirport = argv[i + 1];
+      break;
     case "--leave-date":
-      outboundDateString = argv[i + 1]
-      break
+      outboundDateString = argv[i + 1];
+      break;
     case "--return-date":
-      returnDateString = argv[i + 1]
-      break
+      returnDateString = argv[i + 1];
+      break;
     case "--passengers":
-      adultPassengerCount = argv[i + 1]
-      break
+      adultPassengerCount = argv[i + 1];
+      break;
     case "--individual-deal-price":
-      individualDealPrice = parseInt(argv[i + 1])
-      break
+      individualDealPrice = parseInt(argv[i + 1]);
+      break;
     case "--total-deal-price":
-      totalDealPrice = parseInt(argv[i + 1])
-      break
+      totalDealPrice = parseInt(argv[i + 1]);
+      break;
     case "--interval":
-      interval = parseFloat(argv[i + 1])
-      break
+      interval = parseFloat(argv[i + 1]);
+      break;
   }
-})
+});
 
 // Check if Twilio env vars are set
 const isTwilioConfigured = process.env.TWILIO_ACCOUNT_SID &&
                            process.env.TWILIO_AUTH_TOKEN &&
                            process.env.TWILIO_PHONE_FROM &&
-                           process.env.TWILIO_PHONE_TO
+                           process.env.TWILIO_PHONE_TO;
 
 /**
  * Dashboard renderer
@@ -77,8 +77,8 @@ const isTwilioConfigured = process.env.TWILIO_ACCOUNT_SID &&
 class Dashboard {
 
   constructor() {
-    this.markers = []
-    this.widgets = {}
+    this.markers = [];
+    this.widgets = {};
 
     // Configure blessed
     this.screen = blessed.screen({
@@ -87,16 +87,16 @@ class Dashboard {
       dockBorders: true,
       fullUnicode: true,
       smartCSR: true
-    })
+    });
 
-    this.screen.key(["escape", "q", "C-c"], (ch, key) => process.exit(0))
+    this.screen.key(["escape", "q", "C-c"], (ch, key) => process.exit(0));
 
     // Grid settings
     this.grid = new contrib.grid({
       screen: this.screen,
       rows: 12,
       cols: 12
-    })
+    });
 
     // Graphs
     this.graphs = {
@@ -116,7 +116,7 @@ class Dashboard {
           line: "yellow"
         }
       }
-    }
+    };
 
     // Shared settings
     const shared = {
@@ -130,7 +130,7 @@ class Dashboard {
           fg: "green"
         }
       }
-    }
+    };
 
     // Widgets
     const widgets = {
@@ -197,10 +197,10 @@ class Dashboard {
           }
         })
       }
-    }
+    };
 
     for (let name in widgets) {
-      let widget = widgets[name]
+      let widget = widgets[name];
 
       this.widgets[name] = this.grid.set(
         widget.size.top,
@@ -209,7 +209,7 @@ class Dashboard {
         widget.size.width,
         widget.type,
         widget.options
-      )
+      );
     }
   }
 
@@ -219,7 +219,7 @@ class Dashboard {
    * @return {Void}
    */
   render() {
-    this.screen.render()
+    this.screen.render();
   }
 
   /**
@@ -230,22 +230,22 @@ class Dashboard {
    * @return {Void}
    */
   plot(prices) {
-    const now = format("MM/dd/yy-hh:mm:ss", new Date())
+    const now = format("MM/dd/yy-hh:mm:ss", new Date());
 
     Object.assign(this.graphs.outbound, {
       x: [...this.graphs.outbound.x, now],
       y: [...this.graphs.outbound.y, prices.outbound]
-    })
+    });
 
     Object.assign(this.graphs.return, {
       x: [...this.graphs.return.x, now],
       y: [...this.graphs.return.y, prices.return]
-    })
+    });
 
     this.widgets.graph.setData([
       this.graphs.outbound,
       this.graphs.return
-    ])
+    ]);
   }
 
   /**
@@ -256,26 +256,26 @@ class Dashboard {
    * @return {Void}
    */
   waypoint(data) {
-    this.markers.push(data)
+    this.markers.push(data);
 
     if (this.blink) {
-      return
+      return;
     }
 
     // Blink effect
-    var visible = true
+    var visible = true;
 
     this.blink = setInterval(() => {
       if (visible) {
-        this.markers.forEach((m) => this.widgets.map.addMarker(m))
+        this.markers.forEach((m) => this.widgets.map.addMarker(m));
       } else {
-        this.widgets.map.clearMarkers()
+        this.widgets.map.clearMarkers();
       }
 
-      visible = !visible
+      visible = !visible;
 
-      this.render()
-    }, 1 * TIME_SEC)
+      this.render();
+    }, 1 * TIME_SEC);
   }
 
   /**
@@ -286,8 +286,8 @@ class Dashboard {
    * @return {Void}
    */
   log(messages) {
-    const now = format("MM/dd/yy-hh:mm:ss", new Date())
-    messages.forEach((m) => this.widgets.log.log(`${now}: ${m}`))
+    const now = format("MM/dd/yy-hh:mm:ss", new Date());
+    messages.forEach((m) => this.widgets.log.log(`${now}: ${m}`));
   }
 
   /**
@@ -298,11 +298,11 @@ class Dashboard {
    * @return {Void}
    */
   settings(config) {
-    config.forEach((c) => this.widgets.settings.add(c))
+    config.forEach((c) => this.widgets.settings.add(c));
   }
 }
 
-const dashboard = new Dashboard()
+const dashboard = new Dashboard();
 
 /**
  * Send a text message using Twilio
@@ -313,33 +313,33 @@ const dashboard = new Dashboard()
  */
 const sendTextMessage = (message) => {
   try {
-    const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+    const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
     twilioClient.sendMessage({
       from: process.env.TWILIO_PHONE_FROM,
       to: process.env.TWILIO_PHONE_TO,
       body: message
     }, function(err, data) {
-      if (!dashboard) return
+      if (!dashboard) { return; }
       if (err) {
         dashboard.log([
           chalk.red(`Error: failed to send SMS to ${process.env.TWILIO_PHONE_TO} from ${process.env.TWILIO_PHONE_FROM}`)
-        ])
+        ]);
       } else {
         dashboard.log([
           chalk.green(`Successfully sent SMS to ${process.env.TWILIO_PHONE_TO} from ${process.env.TWILIO_PHONE_FROM}`)
-        ])
+        ]);
       }
-    })
+    });
   } catch(e) {}
-}
+};
 
 /**
- * Fetch latest Southwest prices
+ * fetchPrices latest Southwest prices
  *
  * @return {Void}
  */
-const fetch = () => {
+const fetchPrices = () => {
   osmosis
     .get("https://www.southwest.com")
     .submit(".booking-form--form", {
@@ -358,110 +358,110 @@ const fetch = () => {
     })
     .find("#faresOutbound .product_price")
     .then((priceMarkup) => {
-      const matches = priceMarkup.toString().match(/\$.*?(\d+)/)
-      const price = parseInt(matches[1])
-      fares.outbound.push(price)
+      const matches = priceMarkup.toString().match(/\$.*?(\d+)/);
+      const price = parseInt(matches[1]);
+      fares.outbound.push(price);
     })
     .find("#faresReturn .product_price")
     .then((priceMarkup) => {
-      const matches = priceMarkup.toString().match(/\$.*?(\d+)/)
-      const price = parseInt(matches[1])
-      fares.return.push(price)
+      const matches = priceMarkup.toString().match(/\$.*?(\d+)/);
+      const price = parseInt(matches[1]);
+      fares.return.push(price);
     })
     .done(() => {
-      const lowestOutboundFare = Math.min(...fares.outbound)
-      const lowestReturnFare = Math.min(...fares.return)
-      var faresAreValid = true
+      const lowestOutboundFare = Math.min(...fares.outbound);
+      const lowestReturnFare = Math.min(...fares.return);
+      var faresAreValid = true;
 
       // Clear previous fares
-      fares.outbound = []
-      fares.return = []
+      fares.outbound = [];
+      fares.return = [];
 
       // Get difference from previous fares
-      const outboundFareDiff = prevLowestOutboundFare - lowestOutboundFare
-      const returnFareDiff = prevLowestReturnFare - lowestReturnFare
-      var outboundFareDiffString = ""
-      var returnFareDiffString = ""
+      const outboundFareDiff = prevLowestOutboundFare - lowestOutboundFare;
+      const returnFareDiff = prevLowestReturnFare - lowestReturnFare;
+      var outboundFareDiffString = "";
+      var returnFareDiffString = "";
 
       // Create a string to show the difference
       if (!isNaN(outboundFareDiff) && !isNaN(returnFareDiff)) {
 
         // Usually this is because of a scraping error
         if (!isFinite(outboundFareDiff) || !isFinite(returnFareDiff)) {
-          faresAreValid = false
+          faresAreValid = false;
         }
 
         if (outboundFareDiff > 0) {
-          outboundFareDiffString = chalk.green(`(down \$${Math.abs(outboundFareDiff)})`)
+          outboundFareDiffString = chalk.green(`(down \$${Math.abs(outboundFareDiff)})`);
         } else if (outboundFareDiff < 0) {
-          outboundFareDiffString = chalk.red(`(up \$${Math.abs(outboundFareDiff)})`)
+          outboundFareDiffString = chalk.red(`(up \$${Math.abs(outboundFareDiff)})`);
         } else if (outboundFareDiff === 0) {
-          outboundFareDiffString = chalk.blue(`(no change)`)
+          outboundFareDiffString = chalk.blue(`(no change)`);
         }
 
         if (returnFareDiff > 0) {
-          returnFareDiffString = chalk.green(`(down \$${Math.abs(returnFareDiff)})`)
+          returnFareDiffString = chalk.green(`(down \$${Math.abs(returnFareDiff)})`);
         } else if (returnFareDiff < 0) {
-          returnFareDiffString = chalk.red(`(up \$${Math.abs(returnFareDiff)})`)
+          returnFareDiffString = chalk.red(`(up \$${Math.abs(returnFareDiff)})`);
         } else if (returnFareDiff === 0) {
-          returnFareDiffString = chalk.blue(`(no change)`)
+          returnFareDiffString = chalk.blue(`(no change)`);
         }
       }
 
       if (faresAreValid) {
 
         // Store current fares for next time
-        prevLowestOutboundFare = lowestOutboundFare
-        prevLowestReturnFare = lowestReturnFare
+        prevLowestOutboundFare = lowestOutboundFare;
+        prevLowestReturnFare = lowestReturnFare;
 
         // Do some Twilio magic (SMS alerts for awesome deals)
         const awesomeDealIsAwesome = (
           totalDealPrice && (lowestOutboundFare + lowestReturnFare <= totalDealPrice)
         ) || (
           individualDealPrice && (lowestOutboundFare <= individualDealPrice || lowestReturnFare <= individualDealPrice)
-        )
+        );
 
         if (awesomeDealIsAwesome) {
-          const message = `Deal alert! Combined total has hit \$${lowestOutboundFare + lowestReturnFare}. Individual fares are \$${lowestOutboundFare} (outbound) and \$${lowestReturnFare} (return).`
+          const message = `Deal alert! Combined total has hit \$${lowestOutboundFare + lowestReturnFare}. Individual fares are \$${lowestOutboundFare} (outbound) and \$${lowestReturnFare} (return).`;
 
           // Party time
           dashboard.log([
             rainbow(message)
-          ])
+          ]);
 
           if (isTwilioConfigured) {
-            sendTextMessage(message)
+            sendTextMessage(message);
           }
         }
 
         dashboard.log([
           `Lowest fares for an outbound flight is currently \$${[lowestOutboundFare, outboundFareDiffString].filter(i => i).join(" ")}`,
           `Lowest fares for a return flight is currently \$${[lowestReturnFare, returnFareDiffString].filter(i => i).join(" ")}`
-        ])
+        ]);
 
         dashboard.plot({
           outbound: lowestOutboundFare,
           return: lowestReturnFare
-        })
+        });
       }
 
-      dashboard.render()
+      dashboard.render();
 
-      setTimeout(fetch, interval * TIME_MIN)
-    })
-}
+      setTimeout(fetchPrices, interval * TIME_MIN);
+    });
+};
 
 // Get lat/lon for airports (no validation on non-existent airports)
 airports.forEach((airport) => {
   switch (airport.iata) {
     case originAirport:
-      dashboard.waypoint({ lat: airport.lat, lon: airport.lon, color: "red", char: "X" })
-      break
+      dashboard.waypoint({ lat: airport.lat, lon: airport.lon, color: "red", char: "X" });
+      break;
     case destinationAirport:
-      dashboard.waypoint({ lat: airport.lat, lon: airport.lon, color: "yellow", char: "X" })
-      break
+      dashboard.waypoint({ lat: airport.lat, lon: airport.lon, color: "yellow", char: "X" });
+      break;
   }
-})
+});
 
 // Print settings
 dashboard.settings([
@@ -474,6 +474,6 @@ dashboard.settings([
   `Individual deal price: ${individualDealPrice ? `<= \$${individualDealPrice}` : "disabled"}`,
   `Total deal price: ${totalDealPrice ? `<= \$${totalDealPrice}` : "disabled"}`,
   `SMS alerts: ${isTwilioConfigured ? process.env.TWILIO_PHONE_TO : "disabled"}`
-])
+]);
 
-fetch()
+fetchPrices();
